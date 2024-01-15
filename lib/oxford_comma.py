301b205d07cb2e1ed94e92efead7d89bb9c5c367
Boniface <cheruiyotbony@gmail.com>
2024-01-15 05:39:47 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    x = []
+    if len(items) == 1:
+        x = items[0]
+    elif len(items) == 2:
+        x = ' and '.join(items)
+    else:
+        x = ', '.join(items[:-1]) + ', and ' + items[-1]
+    return x
+
+result = oxford_comma(["Boniface"])
+print(result)
